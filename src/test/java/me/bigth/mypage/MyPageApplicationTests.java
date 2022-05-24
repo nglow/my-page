4d09f1c20280c86437213ecf52a1673e7198cd39
bigth8 <c.bigth@gmail.com>
2022-05-24 17:35:27 +0900

    Initial commit

---
+++ src/test/java/me/bigth/mypage/MyPageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bigth.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
